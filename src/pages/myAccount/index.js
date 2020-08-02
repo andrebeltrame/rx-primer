373d988f9c1752a87b77283ed93a6f0e3a57fd0b
André Beltrame <andrebeltrame@gmail.com>
2020-08-02 13:01:25 -0300

    field forms

--- src/pages/myAccount/index.js
+++ src/pages/myAccount/index.js
@@ -4,31 +4,44 @@ import Input from '../../components/ui/Input'
 import { Button } from '../../components/ui/Buttons'
 
 const MyAccount = () => {
+    const [name , setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [RePassword, setRePassword] = React.useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log(name)
+    }
+
     return (
         <Card className="no-border">
             <h2>Minha Conta</h2>
-            <div className="row">
-                <div className="col col-6">
-                    <Input type="text" label="Nome" />
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col col-6">
+                        <Input value={name} onChange={(event) => setName(event.target.value)} setValue={setName} 
+                         type="text" label="Nome" required/>
+                    </div>
+                    <div className="col col-6">
+                        <Input value={email} setValue={setEmail} onChange={(event) => setEmail(event.target.value)}  type="e-mail" label="E-mail"  required/>
+                    </div>
                 </div>
-                <div className="col col-6">
-                    <Input type="text" label="Sobrenome" />
+                <div className="row">
+                    <div className="col col-6">
+                        <Input value={password} setValue={setPassword} onChange={(event) => setPassword(event.target.value)}  type="password" label="Senha" required/>
+                    </div>
+                    <div className="col col-6">
+                        <Input value={RePassword} setValue={setRePassword} onChange={(event) => setRePassword(event.target.value)}  type="password" label="Repita sua senha" required />
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col col-6">
-                    <Input type="password" label="Senha" />
-                </div>
-                <div className="col col-6">
-                    <Input type="password" label="Repita sua senha" />
-                </div>
-            </div>
 
-            <div className="row">
-                <div className="col col-12">
-                    <Button className="ButtonMd ColorPrimary">Salvar meus dados</Button>
+                <div className="row">
+                    <div className="col col-12">
+                        <Button className="ButtonMd ColorPrimary">Salvar meus dados</Button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </Card>
     )
 }
--- src/pages/new-recipe/index.js
+++ src/pages/new-recipe/index.js
@@ -12,81 +12,109 @@ import './style.css';
 
 const PageNewRecipe = props => {
 
+    //Thumbnail
+    const [recipeName, setRecipeName] = React.useState('');
+    const [time, setTime] = React.useState('');
+    const [qtd, setQtd] = React.useState('');
+    const [isHome, setIsHome] = React.useState('');
+    //Tags
+    const [valNutri, setValNutri] = React.useState('');
+    const [stuff, setStuff] = React.useState('');
+    //image gallery
+    //Steps
+
     const selectedTags = tags => {
         console.log(tags);
     };
 
     return (
-        <div>
-            <Card className="no-border">
-                <h2>Nova Receita</h2>
+      <div>
+        <Card className="no-border">
+          <h2>Nova Receita</h2>
 
-                <div className="row">
-                    <div className="col col-4">
-                        
-                            <Button className="ButtonSm ColorPrimary">Enviar Foto</Button>
-                    </div>
+          <div className="row">
+            <div className="col col-4">
+              <Button className="ButtonSm ColorPrimary">Enviar Foto</Button>
+            </div>
 
-                    <div className="col col-8">
-                        <div className="col-12">
-                            <Input label="Nome da Receita" type="text" required />
-                        </div>
-                        <div className="row">
-                            <div className="col col-6">
-                                <Input label="Tempo de Preparo" type="text" required />
-                            </div>
+            <div className="col col-8">
+              <div className="col-12">
+                <Input
+                  value={recipeName}
+                  onChange={(event) => setRecipeName(event.target.value)}
+                  setValue={recipeName}
+                  label="Nome da Receita"
+                  type="text"
+                  required
+                />
 
-                            <div className="col col-6">
-                                <Input label="Rendimento" type="text" required />
-                            </div>
-
-                            <div className="col-12">
-                                <input type="radio" id="keep-home" name="home" value="home" />
-                                <label for="keep-home">Manter esta receita na home</label>
-                                
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div className="row mb-2">
-                    <h3>Keywords</h3>
-                    <TagsInput selectedTags={selectedTags} tags={['Arroz', 'Feijão']} />
+                {recipeName}
+              </div>
+              <div className="row">
+                <div className="col col-6">
+                  <Input
+                    value={time}
+                    onChange={(event) => setTime(event.target.value)}
+                    setValue={time}
+                    label="Tempo de Preparo"
+                    type="text"
+                    required
+                  />
                 </div>
 
-                <div className="row">
-                    <Tabs>
-                        <TabList>
-                            <Tab>Informação Nutricional</Tab>
-                            <Tab>Ingredientes</Tab>
-                        </TabList>
-
-                        <TabPanel>
-                            <Editor />
-                        </TabPanel>
-                        <TabPanel>
-                            <Editor />
-                        </TabPanel>
-                    </Tabs>
+                <div className="col col-6">
+                  <Input
+                    value={qtd}
+                    onChange={(event) => setQtd(event.target.value)}
+                    setValue={qtd}
+                    label="Rendimento"
+                    type="text"
+                    required
+                  />
                 </div>
 
+                <div className="col-12">
+                  <input
+                    value={isHome}
+                    onChange={(event) => setIsHome(event.target.value)}
+                    setValue={isHome}
+                    type="radio"
+                    id="keep-home"
+                  />
+                  <label for="keep-home">Manter esta receita na home</label>
+                  {isHome}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mb-2">
+            <h3>Keywords</h3>
+            <TagsInput selectedTags={selectedTags} tags={["Arroz", "Feijão"]} />
+          </div>
 
+          <div className="row">
+            <Tabs>
+              <TabList>
+                <Tab>Informação Nutricional</Tab>
+                <Tab>Ingredientes</Tab>
+              </TabList>
 
+              <TabPanel>
+                <Editor/>
+               
+              </TabPanel>
+              <TabPanel>
+                <Editor />
+              </TabPanel>
+            </Tabs>
+          </div>
+          <h3>Galeria de Imagens</h3>
 
-                <h3>Galeria de Imagens</h3>
-       
+          <ImagesUploader />
 
-                <ImagesUploader/>
-
-
-                <Button className="ButtonMd ColorSecundary">Salvar Receita</Button>
-
-            </Card>
-
-
-
-
-        </div>
-    )
+          <Button className="ButtonMd ColorSecundary">Salvar Receita</Button>
+        </Card>
+      </div>
+    );
 }
 export default PageNewRecipe;
